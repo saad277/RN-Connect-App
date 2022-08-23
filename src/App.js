bacfad4f16662c5bc73e3516763fd56a149081b0
Saad <saadsheikh27@gmail.com>
2022-08-23 22:55:20 +0500

    -

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = (props) => {
+    return (
+        <View>
+            <Text>App</Text>
+        </View>
+    );
+};
+
+export default App;
