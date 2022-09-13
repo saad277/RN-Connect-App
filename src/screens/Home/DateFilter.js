019d59388ca5a34ec9e9b3b56dada016d63ce62a
Saad <saadsheikh27@gmail.com>
2022-09-14 02:37:55 +0500

    date feature

--- src/screens/Home/DateFilter.js
+++ src/screens/Home/DateFilter.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 
 import { Icon } from "../../components/Icon";
 import { Text } from "../../components/Text";
 
+const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
 const DateFilter = () => {
+    const [date, setDate] = useState(new Date("2022-06-01"));
+
+    const handleDateIncrement = () => {
+        const result = new Date(new Date(date).setDate(new Date(date).getDate() + 1));
+        setDate(result);
+    };
+    const handleDateDecrement = () => {
+        const result = new Date(new Date(date).setDate(new Date(date).getDate() - 1));
+        setDate(result);
+    };
+
     return (
         <View style={styles.date}>
             <Icon
@@ -12,15 +38,17 @@ const DateFilter = () => {
                 width={14}
                 height={14}
                 style={{ marginRight: 10 }}
-                onPress={() => {}}
+                onPress={handleDateDecrement}
             />
-            <Text>1. June 2022</Text>
+            <Text>
+                {date.getDate()}. {monthNames[date.getMonth()]} {date.getFullYear()}
+            </Text>
             <Icon
                 source={require("../../assets/icons/right-arrow.png")}
                 width={14}
                 height={14}
                 style={{ marginLeft: 10 }}
-                onPress={() => {}}
+                onPress={handleDateIncrement}
             />
         </View>
     );
